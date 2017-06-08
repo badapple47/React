b53aeebd640ff260a3edbd5c810efb9a99bafa50
Pathompong Chaisri <st4rboy@Pathompong-MacBook-Pro.local>
2017-06-08 16:22:55 +0700

    youtube api test

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -10,8 +10,7 @@ class SearchBar extends Component {
   render() {
   return (
     <div>
-      <input onChange={event => this.setState({ term: event.target.value })} />
-      value of the input: {this.state.term}
+      <input onChange={event => this.setState({ term: event.target.value})} />
     </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
+
 import React from 'react' ;
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBknMoPG_SzSjYdaQ6bYupb-ITn-eCjumo';
 
 
+// youtube-api-search for return string data from youtube
+YTSearch({key: API_KEY, term: 'pewdiepie'}, function(data){
+  console.log(data);
+});
+
+
+//pack all component in container name 'app'
 const  App =  () => {
   return ( <div> <SearchBar/> </div> );
 }
 
 
-
+// after package has been packed into one piece called 'app' render 'app' container to HTML
 ReactDOM.render(<App />, document.querySelector('.container'));
